88bbf14983302e2a20ecadb26982cf864d6e52fe
zzzzx10 <zixuan_z@qq.com>
2019-07-01 22:38:56 +0800

    add drawn image base64 output

--- Kafka/detect.py
+++ Kafka/detect.py
@@ -7,13 +7,13 @@ from timeit import default_timer as timer
 from PIL import Image, ImageDraw, ImageFont
 from random import randint
 from io import BytesIO
-
+import keras
 # Streaming imports
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from kafka import KafkaProducer
-
+import random
 # Model imports
 from yolov3_keras.yolo import YOLO
 
@@ -24,7 +24,8 @@ class Spark_Calorie_Calculator():
     def __init__(self,
                  topic_to_consume='instream',
                  topic_for_produce='ourstream',
-                 kafka_endpoint='127.0.0.1:9092'):
+                 kafka_endpoint='127.0.0.1:9092',
+                 model_path='model.h5'):
         """Initialize Spark & TensorFlow environment."""
         self.topic_to_consume = topic_to_consume
         self.topic_for_produce = topic_for_produce
@@ -42,11 +43,13 @@ class Spark_Calorie_Calculator():
         log4jLogger.LogManager.getLogger('akka').setLevel(log_level)
         log4jLogger.LogManager.getLogger('kafka').setLevel(log_level)
         self.logger = log4jLogger.LogManager.getLogger(__name__)
+        self.classifier = keras.models.load_model(model_path)
+        self.classifier._make_predict_function()
 
         # Load Network Model & Broadcast to Worker Nodes
         self.model_od = YOLO()
         #self.model_od_bc = sc.broadcast(model_od)
-
+        self.names = ["class0", "class1", "class2"]
     def start_processing(self):
         zookeeper = "G4master:2181,G401:2181,G402:2181,G403:2181,G404:2181,G405:2181,G406:2181,G407:2181," \
                     "G408:2181,G409:2181,G410:2181,G411:2181,G412:2181,G413:2181,G414:2181,G415:2181"
@@ -79,28 +82,41 @@ class Spark_Calorie_Calculator():
             start = timer()
             print(self.model_od.class_names)
             pre_classes, boxes, single_foods = self.model_od.detect_image(image)
-            end = timer()
-            delta = end - start
+            actual = []
+            for food in single_foods:
+                images = []
+                images.append(food)
+                images = np.array(images)
+                print(images.shape)
+                ingredients, actual_class = self.classifier.predict(images)
+                index = np.argmax(actual_class)
 
-            #get calories
+                actual.append(index)
+            actual = [self.names[x] for x in actual]
+
+            # get calories
             calories = []
-            for dish in pre_classes:
-                import random
+            for dish in actual:
+                #    _,=self.classifier.predict(
                 calorie = random.randint(100, 500)
                 calories.append(calorie)
 
-            drawn_img = self.drawboxes(image, boxes, pre_classes, calories)
+            drawn_img = self.drawboxes(image, boxes,actual, calories)
+            drawn_img_b = base64.b64encode(drawn_img).decode('utf-8')
+            end = timer()
+            delta = end - start
 
             self.logger.info('Done after ' + str(delta) + ' seconds.')
             self.logger.info('Find'+str(len(pre_classes)) + 'dish(s).')
             #TODO: 这里的操作需要用map写吗？后续操作：每个切完片的图进一步预测分类和营养成分，将最终画好的图返回客户端，
-            result={'user': event['user'],
-                    'class': pre_classes[0],
-                    'calories': calories[0],
-                    #'drawn_img': drawn_img,
-                    'process_time': delta}
+            result = {'user': event['user'],
+                    'class': actual,
+                    'calories': calories,
+                    'drawn_img': drawn_img_b,
+                    'process_time': delta
+                    }
             self.outputResult(json.dumps(result))
-    
+
     def outputResult(self, message):
         self.logger.info("Now sending out....")
         self.producer.send(self.topic_for_produce, message.encode('utf-8'))
@@ -114,7 +130,6 @@ class Spark_Calorie_Calculator():
             cls = final_classes[i]
             box = boxes[i]
             cal = calories[i]
-
             label = '{} {}cal'.format(cls, cal)
             draw = ImageDraw.Draw(image)
             label_size = draw.textsize(label, font)
@@ -140,8 +155,8 @@ class Spark_Calorie_Calculator():
 
 if __name__ == '__main__':
     sod = Spark_Calorie_Calculator(
-        topic_to_consume= {"inputImage": 0, "inputImage": 1, "inputImage": 2},
-        topic_for_produce= "outputResult",
+        topic_to_consume={"inputImage": 0, "inputImage": 1, "inputImage": 2},
+        topic_for_produce="outputResult",
         kafka_endpoint="G4master:9092,G401:9092,G402:9092,G403:9092,G404:9092,"
                        "G405:9092,G406:9092,G407:9092,G408:9092,G409:9092,G410:9092,"
                        "G411:9092,G412:9092,G413:9092,G414:9092,G415:9092")
